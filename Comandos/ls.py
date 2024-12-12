cc4661502a93514557917ad600f9eb1e01d0b614
Pruden <prudencosta@gmail.com>
2024-12-12 08:42:08 +0100

    Más comandos, head -n, mv, cp y fix en ls

--- Comandos/ls.py
+++ Comandos/ls.py
@@ -1,8 +1,54 @@
-def formato_ls(contenido):
+import os
+
+RESET = "\033[0m"
+BLUE = "\033[34m"
+BOLD = "\033[1m"
+YELLOW = "\033[33m"
+
+
+def formato_ls(contenido, directorioActual):
     max_len = max(len(item) for item in contenido)
     columnas = 4
     filas = len(contenido) // columnas + (1 if len(contenido) % columnas > 0 else 0)
 
     for i in range(filas):
-        row = [contenido[j] for j in range(i, len(contenido), filas)]
-        print("".join(f"{item:<{max_len}}  " for item in row))
+        row = []
+        for j in range(i, len(contenido), filas):
+            item = contenido[j]
+            item_path = os.getcwd()+directorioActual+ "/"+ item
+            if os.path.isdir(item_path):
+                row.append(f"{BOLD}{BLUE}{item:<{max_len}}{RESET}")
+            else:
+                row.append(f"{item:<{max_len}}{RESET}")
+        print("  ".join(row))
+
+def lsParaMasArchivos(op, directorioActual, lsLen):
+    listados = op.split(" ")
+    if len(listados) > lsLen+1:
+        for l in listados[lsLen:]:
+            file = os.getcwd() + directorioActual + "/" + l
+
+            if os.path.isdir(file):
+
+                if lsLen == 1:
+                    listado = [l for l in os.listdir(file) if not l.startswith(".")]
+                else:
+                    listado = os.listdir(file)
+
+                print(f"{BOLD}{YELLOW}{l}{RESET}:")
+                formato_ls(listado, directorioActual+ "/" + l)
+            else:
+                if os.path.exists(file):
+                    print(f"{BOLD}{YELLOW}{l}{RESET} (no es un directorio):\n{l}")
+                else:
+                    print(f"'{l}' no existe")
+    else:
+        try:
+            listado = os.listdir(os.getcwd() + directorioActual + "/" + listados[lsLen])
+            if lsLen == 1:
+                listado = [l for l in listado if not l.startswith(".")]
+
+            formato_ls(listado, directorioActual+ "/" + listados[lsLen])
+        except Exception:
+            print(listados[lsLen])
+
--- Terminal.py
+++ Terminal.py
@@ -1,18 +1,21 @@
 import os
 import shutil
 import subprocess
+from datetime import datetime
+from Comandos.ls import formato_ls, lsParaMasArchivos
 
-from samba.dcerpc.security import privilege_name
-
-from Comandos.ls import formato_ls
-
-comandos = ["cd", "ls", "mkdir", "pwd"]
+RESET = "\033[0m"
+GREEN = "\033[32m"
+CYAN = "\033[36m"
+BOLD = "\033[1m"
+YELLOW = "\033[33m"
 
 directorioRaiz = "/Terminal"
 directorioActual = os.path.join(directorioRaiz, "home/pruden")
 
 while True:
-    op = input("pruden@Terminal:" + directorioActual + "$ ").strip()
+    prompt = f"{BOLD}{CYAN}pruden@Terminal:{RESET}{GREEN}{directorioActual}{RESET}$ "
+    op = input(prompt).strip()
     if op.startswith("cd"):
         cd = op.split(" ")
         if len(cd) > 2:
@@ -43,6 +46,9 @@ while True:
 
     elif op == "pwd":
         print(directorioActual)
+
+
+        
     elif op.startswith("mkdir"):
         mkdir = op.split(" ")
         if len(mkdir) == 1:
@@ -60,14 +66,16 @@ while True:
 
 
     elif op.startswith("ls"):
-
-
-        if op == "ls":
+        if op.startswith("ls -a "):
+            lsParaMasArchivos(op, directorioActual, 2)
+        elif op.startswith("ls ") and not op.startswith("ls -a"):
+            lsParaMasArchivos(op, directorioActual, 1)
+        elif op == "ls":
             listado = os.listdir(os.getcwd()+ directorioActual)
-            formato_ls([l for l in listado if not l.startswith(".")])
+            formato_ls([l for l in listado if not l.startswith(".")], directorioActual)
         elif op == "ls -a":
             listado = os.listdir(os.getcwd() + directorioActual)
-            formato_ls(listado)
+            formato_ls(listado, directorioActual)
 
     elif op.startswith("touch"):
         if op.startswith("touch "):
@@ -100,10 +108,13 @@ while True:
         else:
             cat = op.split(" ")
             if len(cat) == 2:
-                if os.path.isdir(os.getcwd()+directorioActual+"/"+cat[1]):
+                archivo = os.getcwd()+directorioActual+"/"+cat[1]
+                if os.path.isdir(archivo):
                     print(f"cat: {cat[1]} es un directorio")
+                elif not os.path.exists(archivo):
+                    print(f"cat: '{cat[1]}' no existe")
                 else:
-                    with open(os.getcwd()+directorioActual+"/"+cat[1], "r", encoding="utf-8") as file:
+                    with open(archivo, "r", encoding="utf-8") as file:
                         for l in file.readlines():
                             print(l.strip())
             else:
@@ -142,22 +153,106 @@ while True:
         print(texto[1])
 
     elif op.startswith("cp"):
+        cp = op.split(" ")
+        if op.startswith("cp -r"):
 
+            if len(cp) < 4:
+                print("cp -r: faltan argumentos")
+            elif len(cp) == 4:
+                archivo1 = os.getcwd() + directorioActual + "/" + cp[2]
+                archivo2 = os.getcwd() + directorioActual + "/" + cp[3]
+                if os.path.isdir(archivo1) and not os.path.isdir(archivo2):
+                    if not os.path.exists(archivo2):
+                        shutil.copytree(archivo1, archivo2, dirs_exist_ok=True)
+                    else:
+                        print(f"cp -r: no se puede sobreescribir el no directorio '{cp[3]}' con el directorio '{cp[2]}'")
+                elif os.path.isdir(archivo1) and os.path.isdir(archivo2):
+                    shutil.copytree(archivo1, archivo2, dirs_exist_ok=True)
+                else:
+                    shutil.copy(archivo1, archivo2)
 
-        if op == "cp":
+        elif op == "cp":
             print("cp: faltan argumentos")
         else:
-            cp = op.split(" ")
             if len(cp) == 3:
-                pass
+                try:
+                    archivo1 = os.getcwd()+directorioActual+"/"+cp[1]
+                    archivo2 = os.getcwd()+directorioActual+"/"+cp[2]
+                    if os.path.isdir(archivo1):
+                        print("cp: -r no especificado para copiar un directorio")
+                    else:
+                        shutil.copy(archivo1, archivo2)
+                except Exception:
+                    print(f"cp: no existe el archivo '{cp[1]}'")
+            else:
+                print("cp: espera dos argumentos")
 
+    elif op.startswith("mv"):
+        if op == "cp":
+            print("mv: faltan argumentos")
+        else:
+            mv = op.split(" ")
+            if len(mv) == 3:
+                archivo1 = os.getcwd()+directorioActual+"/"+mv[1]
+                archivo2 = os.getcwd()+directorioActual+"/"+mv[2]
+                if os.path.isdir(archivo2):
+                    shutil.move(archivo1, archivo2)
+                else:
+                    if os.path.isdir(archivo1):
+                        print(f"mv: no se puede sobreescribir el no directorio {mv[2]} con el directorio {mv[1]}")
+                    else:
+                        os.rename(archivo1, archivo2)
+    elif op == "ps -aux" or op == "ps aux":
+        os.system("ps aux")
+    elif op == "ps":
+        os.system("ps")
+    elif op == "ip addr":
+        os.system("ip addr")
+    elif op.startswith("man "):
+        os.system(op)
+    elif op == "top":
+        result = subprocess.run(['ps', '-e', '-o', 'pid,comm'], capture_output=True, text=True)
+        print(result.stdout)
 
     elif op == "clear":
-        #os.system("clear")
+        os.system("clear")
+    elif op == "whoami":
+        print(os.getlogin())
+    elif op == "time":
+        print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    elif op.startswith("firefox"):
+        if len(op.split(" ")) == 2:
+            subprocess.Popen(["firefox",op.split(" ")[1]])
+        elif len(op.split(" ")) > 2:
+            print("firefox: demasiados argumentos")
+        else:
+            subprocess.Popen(["firefox"])
+
+    elif op.startswith("grep "):
         pass
+
+    elif op.startswith("head"):
+        if op.startswith("head -n"):
+            head = op.split(" ")
+            if os.path.exists(os.getcwd()+directorioActual+"/"+head[3]):
+                continuar = True
+                try:
+                    numLineas = int(head[2])
+                except Exception:
+                    continuar = False
+                    print("head: solo acepta parametros enteros")
+
+                if continuar:
+                    archivo = open(os.getcwd()+directorioActual+"/"+head[3], "r", encoding="utf-8")
+
+                    for _ in range(numLineas):
+                        print(archivo.readline().strip())
+
+                    archivo.close()
+
+    elif op == "df":
+        os.system("df")
     elif op == "exit":
         break
     else:
         print(f"{op}: comando no encontrado")
-
-
